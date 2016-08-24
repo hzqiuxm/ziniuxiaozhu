27df0536dad501ba5abbe96d0b6c970f81ad4d16
Yifan Zhang <zousitanghulu@foxmail.com>
2016-08-24 09:42:31 +0800

    紫牛小筑系统架构修改以及功能优化

---
+++ webapp/src/main/webapp/views/js/common/weekreport.js
@@ -0,0 +1,51 @@
+/**
+ * Created by ZhangYifan on 2016/8/12.
+ */
+
+
+
+(function (angular) {
+    'use strict'
+    /**
+     * 用于选课页面
+     *
+     */
+    angular.module('zn-choose', [])
+        .controller('chooseController', ['$scope', function ($scope) {
+
+            $scope.four = true;
+            $scope.template = { name: 'header.html', url: '/views/header.html'}
+
+        }]);
+})(angular)
+$("#confirm").on("click",function(){//登录
+    var url="/views/week-report/report-view.html";
+    if(window.location.hash!=""){
+        url="/views/"+window.location.hash.replace("#/","")+".html"
+    }
+    $.ajax({
+        url: '/weekreport/login',
+        type: 'POST',
+        data:{
+            name:$("#inputid").val(),
+            pwd:$("#inputpwd").val()
+        },
+        dataType: 'html',
+        timeout: 1000,
+        error: function(){console.log('Error loading document');},
+        success: function(result){
+            result= $.parseJSON(result);
+            if(result.result!=0)
+                $("#loaderror").show();
+            else
+                location.href=url;
+        }
+    });
+});
+$("#cancel").on("click",function(){//取消按钮,清空input
+    $(this).parents("#center-div").children("div").children("input").val("");
+});
+
+
+
+
